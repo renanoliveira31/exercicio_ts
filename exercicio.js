1093f1b35295da28ec655faa24c31314878700ee
RENAN FELICIANO OLIVEIRA <108698646+renanoliveira31@users.noreply.github.com>
2024-04-10 17:28:44 -0300

    .

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+function multiplicacao(a, b, c) {
+    return a * b * c;
+}
+function saudar(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+console.log(multiplicacao(3, 5, 18));
+console.log(saudar("Renan"));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicacao(a: number, b: number, c: number): number {
+  return a * b * c;
+}
+
+function saudar(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+console.log(multiplicacao(3, 5, 18));
+console.log(saudar("Renan"));
